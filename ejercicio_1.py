e296c38aa130d8146a3e75da5983f7ae0de00394
Elizondo Iniaki <elizondo.inaki25@gmail.com>
2025-03-24 20:38:55 -0300

    primer commit creado ejercicio 1

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,3 +1,4 @@
 nombre = "Ariel"
 apellido = "Elizondo"
-print(f"Hola, soy{nombre} {apellido} alumno de programacion 1")
+materia = "Programacion 1"
+print(f"Hola, soy{nombre} {apellido} alumno de {materia}")
