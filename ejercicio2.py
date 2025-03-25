2a2740924a87cfd693fe9ef1177a43e80c698ba2
Elizondo Iniaki <elizondo.inaki25@gmail.com>
2025-03-24 21:44:05 -0300

    Nueva rama ejercicio 2 creada

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+nombre =input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+materia = input("Ingrese su materia: ")
+print(f"Hola, soy {nombre} {apellido}, alumno de {materia} ")
